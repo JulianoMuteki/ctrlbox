d2d7fb8fb323b9f85dd18e842842856fc4ceed3e
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-03-15 17:59:23 +0100

    Add - Base Class

---
+++ src/CtrlBox.Domain/Interfaces/Base/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace CtrlBox.Domain.Interfaces.Base
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<T> Repository<T>() where T : class;
+
+        Task<int> Commit();
+
+        void Rollback();
+    }
+}

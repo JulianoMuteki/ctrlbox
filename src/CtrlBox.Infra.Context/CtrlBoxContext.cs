016c90df279285d3323e0f12f734291fa5f13d3f
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-03-15 18:11:06 +0100

    Add - EntityFramework Core 2.3

---
+++ src/CtrlBox.Infra.Context/CtrlBoxContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CtrlBox.Infra.Context
+{
+    public class CtrlBoxContext : DbContext
+    {
+
+        public CtrlBoxContext()
+        {
+
+        }
+
+        public CtrlBoxContext(DbContextOptions<CtrlBoxContext> options)
+             : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}

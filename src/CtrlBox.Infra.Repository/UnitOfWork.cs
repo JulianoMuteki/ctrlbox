c963c1a718e118b3a5815bd46d54d79b115e9286
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-03-15 18:15:43 +0100

    Add - Ioc + Repository

---
+++ src/CtrlBox.Infra.Repository/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using CtrlBox.Domain.Interfaces.Base;
+using CtrlBox.Infra.Context;
+using CtrlBox.Infra.Repository.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CtrlBox.Infra.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly CtrlBoxContext _dbContext;
+        private readonly Dictionary<Type, object> _repositories = new Dictionary<Type, object>();
+
+        public Dictionary<Type, object> Repositories
+        {
+            get { return _repositories; }
+            set { Repositories = value; }
+        }
+
+        public UnitOfWork(CtrlBoxContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IGenericRepository<T> Repository<T>() where T : class
+        {
+            if (Repositories.Keys.Contains(typeof(T)))
+            {
+                return Repositories[typeof(T)] as IGenericRepository<T>;
+            }
+
+            IGenericRepository<T> repo = new GenericRepository<T>(_dbContext);
+            Repositories.Add(typeof(T), repo);
+            return repo;
+        }
+
+        public async Task<int> Commit()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public void Rollback()
+        {
+            _dbContext.ChangeTracker.Entries().ToList().ForEach(x => x.Reload());
+        }
+    }
+}

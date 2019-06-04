7f86283a333fe3150b36e6435b484ed6f72e3549
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-06-04 12:32:28 +0200

    Add - Sale Application Service

--- src/CtrlBox.Application.ViewModel/CheckVM.cs
+++ src/CtrlBox.Application.ViewModel/CheckVM.cs
@@ -4,12 +4,16 @@ using System.Text;
 
 namespace CtrlBox.Application.ViewModel
 {
-   public class CheckVM: ViewModelBase
+    public class CheckVM : ViewModelBase
     {
         public Guid SaleID { get; set; }
         public int Number { get; set; }
         public float Value { get; set; }
-        public DateTime? DtExpire { get; set; }
+        public DateTime? DtExpire
+        {
+            get
+            { return DateTime.ParseExact(DateCheck, "yyyy-MM-dd HH:mm tt", null); }
+        }
 
         public string DateCheck { get; set; }
         public SaleVM Sale { get; set; }

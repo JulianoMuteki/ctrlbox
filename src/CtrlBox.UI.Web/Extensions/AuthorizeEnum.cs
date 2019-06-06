d8296be62b438717b4b3f67e719a3071a217ee77
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-06-06 21:29:36 +0200

    Edit - Inicialize Policy

--- src/CtrlBox.UI.Web/Extensions/AuthorizeEnum.cs
+++ src/CtrlBox.UI.Web/Extensions/AuthorizeEnum.cs
@@ -12,7 +12,7 @@ namespace CtrlBox.UI.Web.Extensions
             if (roles.Any(r => r.GetType().BaseType != typeof(Enum)))
                 throw new ArgumentException("roles");
 
-            this.Roles = string.Join(",", roles.Select(r => Enum.GetName(r.GetType(), r)));
+            this.Roles = string.Join(", ", roles.Select(r => Enum.GetName(r.GetType(), r)));
         }
     }
 }

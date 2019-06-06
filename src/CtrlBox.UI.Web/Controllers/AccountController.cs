738efec764599d4752725707142468868504a740
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-06-06 11:38:46 +0200

    Fix - Forms login

--- src/CtrlBox.UI.Web/Controllers/AccountController.cs
+++ src/CtrlBox.UI.Web/Controllers/AccountController.cs
@@ -55,7 +55,7 @@ namespace CtrlBox.UI.Web.Controllers
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: true);
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");

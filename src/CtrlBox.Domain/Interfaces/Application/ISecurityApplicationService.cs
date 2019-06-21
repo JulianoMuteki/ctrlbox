d02e5ffdc804af58604b24c68f567f7805a28352
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-06-21 11:19:44 +0200

    Add - Dropdown user in create delivery

---
+++ src/CtrlBox.Domain/Interfaces/Application/ISecurityApplicationService.cs
@@ -0,0 +1,12 @@
+﻿using CtrlBox.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CtrlBox.Domain.Interfaces.Application
+{
+    public interface ISecurityApplicationService
+    {
+        IList<ApplicationUser> GetAllUsers();
+    }
+}

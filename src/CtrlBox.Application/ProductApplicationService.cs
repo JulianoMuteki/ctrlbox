4becbb4bad3374b126f79ce0803a63438af0faec
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-06-25 10:29:17 +0200

    Add - Custom exception Product

--- src/CtrlBox.Application/ProductApplicationService.cs
+++ src/CtrlBox.Application/ProductApplicationService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using CtrlBox.Application.ViewModel;
+using CtrlBox.CrossCutting;
 using CtrlBox.Domain.Entities;
 using CtrlBox.Domain.Interfaces.Application;
 using CtrlBox.Domain.Interfaces.Base;
@@ -24,18 +25,29 @@ namespace CtrlBox.Application
 
         public ICollection<ProductVM> GetAll()
         {
-            var products = _unitOfWork.Repository<Product>().GetAll();
-            var productsStocks = _unitOfWork.Repository<StockProduct>().GetAll();
-
-            var productVMs = _mapper.Map<IList<ProductVM>>(products);
-            var productsStocksVMs = _mapper.Map<IList<StockProductVM>>(productsStocks);
-
-            foreach (var product in productVMs)
+            try
             {
-                product.StocksProducts = (from x in productsStocksVMs where x.ProductID.ToString() == product.DT_RowId select x).ToList();
-            }
+                var products = _unitOfWork.Repository<Product>().GetAll();
+                var productsStocks = _unitOfWork.Repository<StockProduct>().GetAll();
 
-            return productVMs;
+                var productVMs = _mapper.Map<IList<ProductVM>>(products);
+                var productsStocksVMs = _mapper.Map<IList<StockProductVM>>(productsStocks);
+
+                foreach (var product in productVMs)
+                {
+                    product.StocksProducts = (from x in productsStocksVMs where x.ProductID.ToString() == product.DT_RowId select x).ToList();
+                }
+
+                return productVMs;
+            }
+            catch (CustomException exc)
+            {
+                throw exc;
+            }
+            catch (Exception ex)
+            {
+                throw CustomException.Create<ClientApplicationService>("Unexpected error fetching get product", nameof(this.GetAll), ex);
+            }
         }
 
         public Task<ICollection<ProductVM>> GetAllAsync()
@@ -45,9 +57,20 @@ namespace CtrlBox.Application
 
         public ProductVM GetById(Guid id)
         {
-            var product = _unitOfWork.Repository<Product>().GetById(id);
-            var productVM = _mapper.Map<ProductVM>(product);
-            return productVM;
+            try
+            {
+                var product = _unitOfWork.Repository<Product>().GetById(id);
+                var productVM = _mapper.Map<ProductVM>(product);
+                return productVM;
+            }
+            catch (CustomException exc)
+            {
+                throw exc;
+            }
+            catch (Exception ex)
+            {
+                throw CustomException.Create<ClientApplicationService>("Unexpected error fetching get product", nameof(this.GetById), ex);
+            }
         }
 
         public Task<ProductVM> GetByIdAsync(Guid id)
@@ -57,36 +80,69 @@ namespace CtrlBox.Application
 
         public ProductVM Add(ProductVM entity)
         {
-            var product = _mapper.Map<Product>(entity);
+            try
+            {
+                var product = _mapper.Map<Product>(entity);
 
-            _unitOfWork.Repository<Product>().Add(product);
-            _unitOfWork.Commit();
+                _unitOfWork.Repository<Product>().Add(product);
+                _unitOfWork.Commit();
 
-            return entity;
+                return entity;
+            }
+            catch (CustomException exc)
+            {
+                throw exc;
+            }
+            catch (Exception ex)
+            {
+                throw CustomException.Create<ClientApplicationService>("Unexpected error fetching add product", nameof(this.Add), ex);
+            }
         }
 
         public Task<ProductVM> AddAsync(ProductVM entity)
         {
-            var product = _mapper.Map<Product>(entity);
+            try
+            {
+                var product = _mapper.Map<Product>(entity);
 
-            _unitOfWork.Repository<Product>().AddAsync(product);
-            _unitOfWork.Commit();
+                _unitOfWork.Repository<Product>().AddAsync(product);
+                _unitOfWork.Commit();
 
-            return Task.FromResult(entity);
+                return Task.FromResult(entity);
+            }
+            catch (CustomException exc)
+            {
+                throw exc;
+            }
+            catch (Exception ex)
+            {
+                throw CustomException.Create<ClientApplicationService>("Unexpected error fetching add delivery", nameof(this.AddAsync), ex);
+            }
         }
 
         public ProductVM Update(ProductVM updated)
         {
-            var product = _mapper.Map<Product>(updated);
-            Product productUpdate = _unitOfWork.Repository<Product>().GetById(product.Id);
-
-            if (productUpdate != null)
+            try
             {
-                productUpdate.UpdateData(product);
-                _unitOfWork.Repository<Product>().Update(productUpdate);
-                _unitOfWork.Commit();
+                var product = _mapper.Map<Product>(updated);
+                Product productUpdate = _unitOfWork.Repository<Product>().GetById(product.Id);
+
+                if (productUpdate != null)
+                {
+                    productUpdate.UpdateData(product);
+                    _unitOfWork.Repository<Product>().Update(productUpdate);
+                    _unitOfWork.Commit();
+                }
+                return updated;
             }
-            return updated;
+            catch (CustomException exc)
+            {
+                throw exc;
+            }
+            catch (Exception ex)
+            {
+                throw CustomException.Create<ClientApplicationService>("Unexpected error fetching update delivery", nameof(this.Update), ex);
+            }
         }
 
         public Task<ProductVM> UpdateAsync(ProductVM updated)
@@ -96,13 +152,24 @@ namespace CtrlBox.Application
 
         public void Delete(Guid id)
         {
-            var product = _unitOfWork.Repository<Product>().GetById(id);
-
-            if (product != null)
+            try
             {
+                var product = _unitOfWork.Repository<Product>().GetById(id);
 
-                _unitOfWork.Repository<Product>().Delete(product);
-                _unitOfWork.Commit();
+                if (product != null)
+                {
+
+                    _unitOfWork.Repository<Product>().Delete(product);
+                    _unitOfWork.Commit();
+                }
+            }
+            catch (CustomException exc)
+            {
+                throw exc;
+            }
+            catch (Exception ex)
+            {
+                throw CustomException.Create<ClientApplicationService>("Unexpected error fetching delete delivery", nameof(this.Delete), ex);
             }
         }
 
@@ -113,11 +180,22 @@ namespace CtrlBox.Application
 
         public ICollection<ClientProductValueVM> ConnectRouteToClient(ICollection<ClientProductValueVM> clientsProductsVMs)
         {
-            var clientsProducts = _mapper.Map<IList<ClientProductValue>>(clientsProductsVMs);
-            _unitOfWork.Repository<ClientProductValue>().AddRange(clientsProducts);
-            _unitOfWork.Commit();
+            try
+            {
+                var clientsProducts = _mapper.Map<IList<ClientProductValue>>(clientsProductsVMs);
+                _unitOfWork.Repository<ClientProductValue>().AddRange(clientsProducts);
+                _unitOfWork.Commit();
 
-            return clientsProductsVMs;
+                return clientsProductsVMs;
+            }
+            catch (CustomException exc)
+            {
+                throw exc;
+            }
+            catch (Exception ex)
+            {
+                throw CustomException.Create<ClientApplicationService>("Unexpected error fetching connect Route to Client", nameof(this.ConnectRouteToClient), ex);
+            }
         }
 
         public int AddProductStock(ICollection<StockProductVM> stocksProductsVM)
@@ -140,64 +218,123 @@ namespace CtrlBox.Application
 
                 return 1;
             }
+            catch (CustomException exc)
+            {
+                throw exc;
+            }
             catch (Exception ex)
             {
-                throw ex;
+                throw CustomException.Create<ClientApplicationService>("Unexpected error fetching add product stock", nameof(this.AddProductStock), ex);
             }
         }
 
         public ICollection<StockProductVM> GetProductsStock()
         {
-            var productsStock = _unitOfWork.Repository<StockProduct>().GetAll();
-
-            var products = _unitOfWork.Repository<Product>().GetAll();
-            var productsWithoutStock = products.Where(p => !productsStock.Any(sp => sp.ProductID == p.Id)).ToList();
-
-            foreach (var product in productsWithoutStock)
+            try
             {
-                productsStock.Add(new StockProduct()
-                {
-                    ProductID = product.Id,
-                    Amount = 0,
-                    Product = product
-                });
-            }
+                var productsStock = _unitOfWork.Repository<StockProduct>().GetAll();
 
-            var stocksProductsVMs = _mapper.Map<IList<StockProductVM>>(productsStock);
-            return stocksProductsVMs;
+                var products = _unitOfWork.Repository<Product>().GetAll();
+                var productsWithoutStock = products.Where(p => !productsStock.Any(sp => sp.ProductID == p.Id)).ToList();
+
+                foreach (var product in productsWithoutStock)
+                {
+                    productsStock.Add(new StockProduct()
+                    {
+                        ProductID = product.Id,
+                        Amount = 0,
+                        Product = product
+                    });
+                }
+
+                var stocksProductsVMs = _mapper.Map<IList<StockProductVM>>(productsStock);
+                return stocksProductsVMs;
+            }
+            catch (CustomException exc)
+            {
+                throw exc;
+            }
+            catch (Exception ex)
+            {
+                throw CustomException.Create<ClientApplicationService>("Unexpected error fetching get product stock", nameof(this.GetProductsStock), ex);
+            }
         }
 
         public ICollection<DeliveryProductVM> GetDeliveryProducts(Guid deliveryID)
         {
-            var deliveries = _unitOfWork.RepositoryCustom<IDeliveryRepository>().GetDeliveryProductsLoad(deliveryID);
+            try
+            {
+                var deliveries = _unitOfWork.RepositoryCustom<IDeliveryRepository>().GetDeliveryProductsLoad(deliveryID);
 
-            var deliveriesVMs = _mapper.Map<IList<DeliveryProductVM>>(deliveries);
-            return deliveriesVMs;
+                var deliveriesVMs = _mapper.Map<IList<DeliveryProductVM>>(deliveries);
+                return deliveriesVMs;
+            }
+            catch (CustomException exc)
+            {
+                throw exc;
+            }
+            catch (Exception ex)
+            {
+                throw CustomException.Create<ClientApplicationService>("Unexpected error fetching get delivery delivery", nameof(this.GetDeliveryProducts), ex);
+            }
         }
 
         public ICollection<ClientProductValueVM> GetClientsProductsByClientID(Guid clientID)
         {
-            var clientsProductsValue = _unitOfWork.Repository<ClientProductValue>().FindAll(x => x.ClientID == clientID);
+            try
+            {
+                var clientsProductsValue = _unitOfWork.Repository<ClientProductValue>().FindAll(x => x.ClientID == clientID);
 
-            var clientsProductsValueVMs = _mapper.Map<IList<ClientProductValueVM>>(clientsProductsValue);
-            return clientsProductsValueVMs;
+                var clientsProductsValueVMs = _mapper.Map<IList<ClientProductValueVM>>(clientsProductsValue);
+                return clientsProductsValueVMs;
+            }
+            catch (CustomException exc)
+            {
+                throw exc;
+            }
+            catch (Exception ex)
+            {
+                throw CustomException.Create<ClientApplicationService>("Unexpected error fetching add client product value", nameof(this.GetClientsProductsByClientID), ex);
+            }
         }
 
         public StockVM GetStock()
         {
-            var stock = _unitOfWork.Repository<Stock>().GetAll().FirstOrDefault();
+            try
+            {
+                var stock = _unitOfWork.Repository<Stock>().GetAll().FirstOrDefault();
 
-            var stockVM = _mapper.Map<StockVM>(stock);
-            return stockVM;
+                var stockVM = _mapper.Map<StockVM>(stock);
+                return stockVM;
+            }
+            catch (CustomException exc)
+            {
+                throw exc;
+            }
+            catch (Exception ex)
+            {
+                throw CustomException.Create<ClientApplicationService>("Unexpected error fetching add stock", nameof(this.GetStock), ex);
+            }
         }
 
         public void AddStock(int stockTotal)
         {
-            Stock stock = new Stock();
-            stock.AmountBoxes = stockTotal;
+            try
+            {
+                Stock stock = new Stock();
+                stock.AmountBoxes = stockTotal;
 
-            _unitOfWork.Repository<Stock>().Add(stock);
-            _unitOfWork.Commit();
+                _unitOfWork.Repository<Stock>().Add(stock);
+                _unitOfWork.Commit();
+            }
+            catch (CustomException exc)
+            {
+                throw exc;
+            }
+            catch (Exception ex)
+            {
+                throw CustomException.Create<ClientApplicationService>("Unexpected error fetching add stock", nameof(this.AddStock), ex);
+            }
         }
     }
 }

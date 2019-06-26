c92cbad92d9ee2647384520bbbdbfcd2613eb810
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-06-26 18:47:25 +0200

    Add - Service and repository

---
+++ src/CtrlBox.Application/PaymentApplicationService.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using CtrlBox.Application.ViewModel;
+using CtrlBox.Domain.Interfaces.Application;
+using CtrlBox.Domain.Interfaces.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtrlBox.Application
+{
+    public class PaymentApplicationService : IPaymentApplicationService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public PaymentApplicationService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public PaymentVM Add(PaymentVM entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PaymentVM> AddAsync(PaymentVM entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guid> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<PaymentVM> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<PaymentVM>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PaymentVM GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PaymentVM> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PaymentVM Update(PaymentVM updated)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PaymentVM> UpdateAsync(PaymentVM updated)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/CtrlBox.Domain/Interfaces/Application/IPaymentApplicationService.cs
@@ -0,0 +1,11 @@
+﻿using CtrlBox.Application.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CtrlBox.Domain.Interfaces.Application
+{
+    public interface IPaymentApplicationService : IApplicationServiceBase<PaymentVM>
+    {
+    }
+}

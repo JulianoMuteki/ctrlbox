8e2ef775e701f70f8ee9844d1b0ceb5101e244a9
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-07-02 11:44:29 +0200

    Add - View end controller address

---
+++ src/CtrlBox.Application.ViewModel/AddressVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CtrlBox.Application.ViewModel
+{
+    public class AddressVM : ViewModelBase
+    {
+        public string CEP { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string District { get; set; }
+        public string Estate { get; set; }
+        public string Reference { get; set; }
+    }
+}
---
+++ src/CtrlBox.UI.Web/Controllers/AddressController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CtrlBox.UI.Web.Controllers
+{
+    public class AddressController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}

db192b88da20787b7ede0f03f9793d818da157b1
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-07-02 14:53:53 +0200

    Add - Address map

---
+++ src/CtrlBox.Infra.Context/Mapping/AddressMap.cs
@@ -0,0 +1,42 @@
+﻿using CtrlBox.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CtrlBox.Infra.Context.Mapping
+{
+    public class AddressMap : EntityConfiguration<Address>
+    {
+        protected override void Initialize(EntityTypeBuilder<Address> builder)
+        {
+            base.Initialize(builder);
+
+            builder.ToTable("Addresses");
+            builder.Property(x => x.Id).HasColumnName("AddressID");
+            builder.HasKey(b => b.Id).HasName("AddressID");
+
+            builder.Property(e => e.CEP)
+                .IsRequired()
+                .HasMaxLength(15);
+
+            builder.Property(e => e.Street)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            builder.Property(e => e.Number)
+                .IsRequired()
+                .HasMaxLength(15);
+
+            builder.Property(e => e.District)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            builder.Property(e => e.Estate)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(e => e.Reference)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}

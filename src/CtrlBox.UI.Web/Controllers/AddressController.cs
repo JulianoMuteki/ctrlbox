ef00e7d0376a43db43d0c98ee642cd97fa026667
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-07-03 11:36:37 +0200

    Add - Save address

--- src/CtrlBox.UI.Web/Controllers/AddressController.cs
+++ src/CtrlBox.UI.Web/Controllers/AddressController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using CtrlBox.Application.ViewModel;
+using CtrlBox.Domain.Interfaces.Application;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CtrlBox.UI.Web.Controllers
 {
     public class AddressController : Controller
     {
+        private readonly IAddressApplicationService _clientApplicationService;
+        private readonly IMapper _mapper;
+
+        public AddressController(IAddressApplicationService clientService, IMapper mapper)
+        {
+            _clientApplicationService = clientService;
+            _mapper = mapper;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,6 +24,13 @@ namespace CtrlBox.UI.Web.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public  IActionResult Create(AddressVM addressVM)
+        {
+            _clientApplicationService.Add(addressVM);
+            return RedirectToAction("Index");
         }
     }
 }

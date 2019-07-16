8db304f139996fd073d7b00181583041a038ddd8
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-07-16 14:55:55 +0200

    Add - Create Box

--- src/CtrlBox.UI.Web/Controllers/BoxController.cs
+++ src/CtrlBox.UI.Web/Controllers/BoxController.cs
@@ -2,22 +2,65 @@
 using CtrlBox.CrossCutting;
 using CtrlBox.Domain.Interfaces.Application;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
+using System.Linq;
 
 namespace CtrlBox.UI.Web.Controllers
 {
     public class BoxController : Controller
     {
         private readonly IBoxApplicationService _boxApplicationService;
+        private readonly IProductApplicationService _productApplicationService;
 
-        public BoxController(IBoxApplicationService boxApplicationService)
+        public BoxController(IBoxApplicationService boxApplicationService, IProductApplicationService productApplicationService)
         {
             _boxApplicationService = boxApplicationService;
-                
+            _productApplicationService = productApplicationService;
+
         }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Create()
+        {
+            var boxesType = _boxApplicationService.GetAllBoxesType()
+                                                    .Select(boxType => new SelectListItem
+                                                    {
+                                                        Value = boxType.DT_RowId,
+                                                        Text = boxType.Name
+                                                    }).ToList();
+            ViewData["BoxesType"] = boxesType;
+
+            var products = _productApplicationService.GetAll()
+                                        .Select(prod => new SelectListItem
+                                        {
+                                            Value = prod.DT_RowId,
+                                            Text = $"{prod.Name} {prod.UnitMeasure}"
+                                        }).ToList();
+            ViewData["Products"] = products;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(BoxVM boxTypeVM)
+        {
+            try
+            {
+
+
+                return RedirectToAction("Index");
+            }
+            catch (CustomException exc)
+            {
+                throw exc;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public IActionResult BoxesType()

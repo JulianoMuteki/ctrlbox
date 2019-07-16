14f8cb47e62ae3f19ac0f72547ee8e625bfd74be
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-07-16 11:18:55 +0200

    Add - Boxes type view

--- src/CtrlBox.UI.Web/Controllers/BoxController.cs
+++ src/CtrlBox.UI.Web/Controllers/BoxController.cs
@@ -1,14 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CtrlBox.UI.Web.Controllers
 {
     public class BoxController : Controller
     {
+        private readonly IMapper _mapper;
+
+        public BoxController()
+        {
+
+        }
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BoxesType()
+        {
+            return View();
+        }
+
+        public IActionResult GetAjaxHandlerBoxesType()
         {
             return View();
         }
---
+++ src/CtrlBox.UI.Web/wwwroot/scripts/controllerJS/views/box-type.js
@@ -0,0 +1,40 @@
+﻿var BoxTypeComponents = function () {
+    var oTableBox;
+
+    function initPage() {
+        inicializateComponentes();
+    }
+
+    function inicializateComponentes() {
+        oTableBox = $('#tbBoxesType').dataTable({
+            "sAjaxSource": 'Box/GetAjaxHandlerBoxesType',
+            "bProcessing": true,
+            "bDestroy": true,
+            "aoColumns": [
+                        {
+                            "mData": "Name",
+                            "bSortable": false
+                        },
+                        {
+                            "mData": "Description",
+                            "bSortable": false
+                        },
+                        {
+                            "bSortable": false,
+                            "defaultContent": "<i>Edit Not set</i>",
+                        }
+            ]
+        });
+    }
+
+    return {
+        //main function to initiate the module
+        init: function () {
+            initPage();
+        },
+        readOnly: function () {
+            $('#tbBoxesType').dataTable();
+
+        }
+    };
+}();

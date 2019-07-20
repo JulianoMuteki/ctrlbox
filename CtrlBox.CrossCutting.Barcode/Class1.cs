67e038c6789fd57b27621f9a3dba3ecc0720c650
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-07-20 15:18:33 +0200

    Add - Barcode

---
+++ CtrlBox.CrossCutting.Barcode/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CtrlBox.CrossCutting.Barcode
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            //Install-Package BarcodeLib -Version 2.2.2
+            BarcodeLib.Barcode b = new BarcodeLib.Barcode();
+            
+        }
+    }
+}

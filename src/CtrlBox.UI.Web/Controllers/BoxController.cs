9bc0143cf456f3b55c9c6eb382b0202ac1cc4f01
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-07-23 17:26:22 +0200

    Edit - Text dropdown box types

--- src/CtrlBox.UI.Web/Controllers/BoxController.cs
+++ src/CtrlBox.UI.Web/Controllers/BoxController.cs
@@ -79,7 +79,7 @@ namespace CtrlBox.UI.Web.Controllers
                                                     .Select(boxType => new SelectListItem
                                                     {
                                                         Value = boxType.DT_RowId,
-                                                        Text = boxType.Name
+                                                        Text = $"{boxType.Name} - {boxType.Description}"
                                                     }).ToList();
             ViewData["BoxesType"] = boxesType;
 

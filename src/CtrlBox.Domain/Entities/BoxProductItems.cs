0221cc3edbe2944402bfc61b3221fb23710fc110
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2019-10-22 11:22:38 +0200

    Add - class BoxProductItems

---
+++ src/CtrlBox.Domain/Entities/BoxProductItems.cs
@@ -0,0 +1,38 @@
+﻿using CtrlBox.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CtrlBox.Domain.Entities
+{
+    public class BoxProductItems : ValueObject<BoxProductItems>
+    {
+        public Box Box { get; private set; }
+        public Product Product { get; private set; }
+
+        public Guid ProductID { get; private set; }
+        public Guid BoxID { get; private set; }
+
+        public int TotalItems { get; private set; }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            return this.GetType().GetProperties().Select(propInfo => propInfo.GetValue(this, null));
+        }
+
+
+        private BoxProductItems()
+        {
+
+        }
+
+        public static BoxProductItems FactoryCreate(Guid boxID, Guid productID)
+        {
+            return new BoxProductItems()
+            {
+                BoxID = boxID,
+                ProductID = productID
+            };
+        }
+    }
+}
